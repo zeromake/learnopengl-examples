1cdc0aa03d8c78dae43d777bd375372c70397e36
Petr Abdulin <pabdulin@gmail.com>
2024-04-06 17:14:38 +0700

    Updated sokol_gfx_imgui usage

--- libs/dbgui/dbgui.cc
+++ libs/dbgui/dbgui.cc
@@ -15,12 +15,12 @@
 
 extern "C" {
 
-static sg_imgui_t sg_imgui;
+static sgimgui_t sg_imgui;
 
 void __dbgui_setup(int sample_count) {
     // setup debug inspection header(s)
-    const sg_imgui_desc_t desc = { };
-    sg_imgui_init(&sg_imgui, &desc);
+    const sgimgui_desc_t desc = { };
+    sgimgui_init(&sg_imgui, &desc);
 
     // setup the sokol-imgui utility header
     simgui_desc_t simgui_desc = { };
@@ -30,7 +30,7 @@ void __dbgui_setup(int sample_count) {
 }
 
 void __dbgui_shutdown(void) {
-    sg_imgui_discard(&sg_imgui);
+    sgimgui_discard(&sg_imgui);
     simgui_shutdown();
 }
 
@@ -38,19 +38,20 @@ void __dbgui_draw(void) {
     simgui_new_frame({ sapp_width(), sapp_height(), sapp_frame_duration(), sapp_dpi_scale() });
     if (ImGui::BeginMainMenuBar()) {
         if (ImGui::BeginMenu("sokol-gfx")) {
-            ImGui::MenuItem("Capabilities", 0, &sg_imgui.caps.open);
-            ImGui::MenuItem("Buffers", 0, &sg_imgui.buffers.open);
-            ImGui::MenuItem("Images", 0, &sg_imgui.images.open);
-            ImGui::MenuItem("Samplers", 0, &sg_imgui.samplers.open);
-            ImGui::MenuItem("Shaders", 0, &sg_imgui.shaders.open);
-            ImGui::MenuItem("Pipelines", 0, &sg_imgui.pipelines.open);
-            ImGui::MenuItem("Passes", 0, &sg_imgui.passes.open);
-            ImGui::MenuItem("Calls", 0, &sg_imgui.capture.open);
+            ImGui::MenuItem("Capabilities", 0, &sg_imgui.caps_window.open);
+            ImGui::MenuItem("Buffers", 0, &sg_imgui.buffer_window.open);
+            ImGui::MenuItem("Images", 0, &sg_imgui.image_window.open);
+            ImGui::MenuItem("Samplers", 0, &sg_imgui.sampler_window.open);
+            ImGui::MenuItem("Shaders", 0, &sg_imgui.shader_window.open);
+            ImGui::MenuItem("Pipelines", 0, &sg_imgui.pipeline_window.open);
+            // NOTE(pabdulin): see https://github.com/floooh/sokol/blob/7f7cd64c6d9d1d4ed08d88a3879b1d69841bf0a4/CHANGELOG.md?plain=1#L69
+            // ImGui::MenuItem("Passes", 0, &sg_imgui.passes.open);
+            ImGui::MenuItem("Calls", 0, &sg_imgui.capture_window.open);
             ImGui::EndMenu();
         }
         ImGui::EndMainMenuBar();
     }
-    sg_imgui_draw(&sg_imgui);
+    sgimgui_draw(&sg_imgui);
     simgui_render();
 }
 
