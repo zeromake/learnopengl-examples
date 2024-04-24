a984d44a8c7fe84a3897e8fbcf416f66467a773a
zeromake <a390720046@gmail.com>
2024-04-24 19:24:00 +0800

    feat: sokol-shdc move to xrepo

--- xmake.lua
+++ xmake.lua
@@ -1,64 +1,5 @@
 add_rules("mode.release", "mode.debug")
 
-package("sokol-shdc")
-    set_kind("binary")
-    set_license("MIT")
-    set_homepage("https://github.com/floooh/sokol-tools")
-    local hash = "74c6bb111d51121fa288f1787c9df5a56545bbfd"
-    local url_prefix = "https://github.com/floooh/sokol-tools-bin/raw/"..hash.."/bin/"
-    local url_suffix = "win32/sokol-shdc.exe"
-    if is_host("macosx") then
-        if os.arch() == "arm64" then
-            url_suffix = "osx_arm64/sokol-shdc"
-            add_versions("latest", "ae4064824ea079d10cdc5e0aef8e3a11308ef4acc0b64add651194620f5f7037")
-        else
-            url_suffix = "osx/sokol-shdc"
-            add_versions("latest", "b8f263b9e08f6e62bd6f0c061922243c8b00cc2a02770f47185d65bf8f2eddfc")
-        end
-    elseif is_host("linux") then 
-        url_suffix = "linux/sokol-shdc"
-        add_versions("latest", "fffc93a057ae27fbdf98822a87a7419cdcda3163a3842b65da2a14b886cc15a5")
-    else
-        add_versions("latest", "ff0faf3547996078b037816387b3c84874b447b29f0f1a09088b1c06822c91bd")
-    end
-    set_urls(url_prefix..url_suffix)
-    on_install(function (package) 
-        local bin = package:installdir("bin")
-        if is_host("windows") then
-            os.cp("../sokol-shdc.exe", bin)
-        else
-            os.run("chmod 755 ../sokol-shdc")
-            os.cp("../sokol-shdc", bin)
-        end
-    end)
-    on_test(function (package)
-        os.vrun("sokol-shdc --help")
-    end)
-package_end()
-
-rule("sokol_shader")
-    set_extensions(".glsl")
-    on_buildcmd_file(function (target, batchcmds, sourcefile, opt)
-        import("lib.detect.find_tool")
-        local sokolshdc = find_tool("sokol-shdc", {check = "--help"})
-        local targetfile = path.relative(sourcefile, "src")
-        batchcmds:mkdir(path.join("$(buildir)/sokol_shader", path.directory(targetfile)))
-        local targetfile = vformat(path.join("$(buildir)/sokol_shader", targetfile..".h"))
-        batchcmds:vrunv(sokolshdc.program, {
-            "--ifdef",
-            "-l",
-            "hlsl5:glsl330:glsl300es:metal_macos:metal_ios",
-            "--input",
-            sourcefile,
-            "--output",
-            targetfile,
-        })
-        batchcmds:show_progress(opt.progress, "${color.build.object}glsl %s", sourcefile)
-        batchcmds:add_depfiles(sourcefile)
-    end)
-rule_end()
-
-add_rules("sokol_shader")
 
 add_repositories("zeromake https://github.com/zeromake/xrepo.git")
 
@@ -101,6 +42,7 @@ end
 target("shader")
     set_kind("object")
     add_packages("sokol-shdc")
+    add_rules("@sokol-shdc/shader")
     for _, f in ipairs(os.files("src/**/*.glsl")) do
         add_files(f)
     end
